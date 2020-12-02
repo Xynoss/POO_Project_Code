617026e3cc81cd89ffdfd29d7a8ccc16da549b58
yannicko10 <74901205+yannicko10@users.noreply.github.com>
2020-12-02 12:47:01 +0100

    Update CL_map__Stock.h

--- Projet_POO_Application/Projet_POO_Application/CL_map__Stock.h
+++ Projet_POO_Application/Projet_POO_Application/CL_map__Stock.h
@@ -4,36 +4,29 @@ namespace NS_Composants {
 	ref class CL_map_Stock
 	{
 	private:
-		int TauxTVA;
-		int TVA;
+		int ID_Article;
 		int PrixUnitaire;
-		int nmbArticle;
-		int SeuilApprovisionnement;
 		String^ NatureArticle;
 		String^ NomArticle;
-		String^ ReferenceArtcicle;
+		String^ RefArtcicle;
+		String^ CouleurArticle;
 	public:
 		CL_map_Stock(void);
 		String^ SELECT(void);
 		String^ INSERT(void);
 		String^ UPDATE(void);
 		String^ DELETE(void);
-		String^ SELECT_STOCK(void);
-		void setTauxTVA(int);
-		void setTVA(int);
-		void setPrixUnitaire(int);
-		void setnmbArticle(int);
-		void setSeuilApprovisionnement(int);
+		void setIDArticle(int);
+		void setNombreStock(int);
 		void setNomArticle(String^);
 		void setNatureArticle(String^);
-		void setReferenceArticle(String^);
-		int getTauxTVA();
-		int getTVA();
-		int getPrixUnitaire();
-		int getSeuilApprovisionnement();
-		int getnmbArticle();
+		void setRefArticle(String^);
+		void setCouleurArticle(String^);
+		int getIDArticle();
+		int getNombreStock();
 		String^ getNomArticle(void);
-		String^ getReferenceArticle(void);
+		String^ getRefArticle(void);
 		String^ getNatureArticle(void);
+		String^ getCouleurArticle(void);
 	};
 }
