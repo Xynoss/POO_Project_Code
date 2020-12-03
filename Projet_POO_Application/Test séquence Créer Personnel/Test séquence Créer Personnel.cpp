49f5b10aa15f0ed8972a4b2227f7bd29009ac0fc
Bleidwyn <74651888+Bleidwyn@users.noreply.github.com>
2020-12-03 15:47:20 +0100

    Test de test

---
+++ Projet_POO_Application/Test séquence Créer Personnel/Test séquence Créer Personnel.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <string>
+#include <iostream>
+#include "CppUnitTest.h"
+#include "../Projet_POO_Application/CL_CAD.h"
+#include "../Projet_POO_Application/CL_map_TBPERSONNEL.h"
+#include "../Projet_POO_Application/CL_svc_gestionPersonnel.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace TestséquenceCréerPersonnel
+{
+	TEST_CLASS(TestséquenceCréerPersonnel)
+	{
+	public:
+		
+		TEST_METHOD(TestMethodAjouterSSup)
+		{
+			string nom = "Dupet";
+			string prenom = "Benoit";
+			string adresse = "17, Allée des Cerisiers";
+			string date = "2005/05/13";
+			
+
+			
+		}
+	};
+}
