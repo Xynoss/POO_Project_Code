2be485cb8aa025ec86175f242a2df0c44c76fbdd
NathanaelDefo <74901693+NathanaelDefo@users.noreply.github.com>
2020-12-03 06:28:57 +0100

    Update Commande.cpp

--- Commande.cpp
+++ Commande.cpp
@@ -5,73 +5,93 @@ namespace NS_Composants
 {
 	Commande::Commande(void)
 	{
-		this->Reference ="Rien";
-		this->DateCommande = "RIEN";
-		this->DateLivraison = "RIEN";
-		this->DatePayement = "RIEN";
-		this->MoyenPayement = "RIEN";
-		this->DateSolde = "RIEN";
-		this->MontantHT = 0;
-		this->MontantTVA = 0;
-		this->MontantTTC = 0;
+		this->ID_Article =-1;
+		this->ID_Facture = -1;
+		this->QuantitéArticle =0;
+		this->PrixUnitaire = 0;
+		
 	}
 
 	String^ Commande::SELECT(void)
 	{
-		return "SELECT Reference, DateCommande, DateLivraison,DatePayement,MoyenPayement,DateSolde,MontantHT,MontantTVA,MontantTTC" + "FROM Commande;";
+		return "SELECT `Commande`.`ID_Facture`,`commande`.`QuantitéArticle`,`Commande`.`PrixUnitaire`, `Facture`.* , `Date`.`ID_Date`, `Date`.`DatePaiment`,`Date`.`MontantPayment`,`Date`.`MoyenPayment`,FROM `Commande` , `Facture`, `Date` WHERE `Commande`.`ID_Facture` = `Facture`.`ID_Facture` AND `Facture`.`ID_Facture` = `Date`.`ID_Facture` ORDER BY `Commande`.`ID_Facture` ASC";
 	}
 
-	String^ Commande::INSERT(void)
+	String^ Commande::INSERTCommande(int ID_Article, int ID_Facture)
 	{
-		return "INSERT INTO Commande " + "( Reference, DateCommande, DateLivraison,DatePayement,MoyenPayement,DateSolde,MontantHT,MontantTVA,MontantTTC) " + "VALUES('" + this->getReference() + "', '" + this->getDateCommande() + "', '" + this->getDateLivraison() + "', '" + this->getDatePayement() + "', '" + this->getMoyenPayement() + "', '" + this->getDateSolde() + "', '" + this->getMontantHT() + "', '" + this->getMontantTVA() + "', '" + this->getMontantTTC() + "');SELECT @@IDENTITY;";
+		return "INSERT INTO Commande " + "( `ID_Article`, `ID_Facture`, `QuantitéArticle`,`PrixUnitaire`) " + "VALUES('" + this->getID_Article() + "', '" + this->getID_Facture() + "', '" + this->getQuantitéArticle() + "', '" + this->getPrixUnitaire()  + "');SELECT @@IDENTITY;";
 	}
+	String^ Commande::INSERTFacture(int ID_client)
+	{
+		return "INSERT INTO Facture " + "( `RefCommande`, `MontantTVA`,`DateLivraison`,`Remise`,`DateSolde`,`ID_Client`) " + "VALUES('" + this->getRefCommande() + "', '" + this->getMontantTVA() + "', '" + this->getRemise() + "','" + this->getDateSolde() + "', '" + ID_client + "');SELECT @@IDENTITY;";
+	}
+	String^ Commande::INSERTDate(int ID_Facture)
+	{
+		return "INSERT INTO Date " + "( `DatePaiment`, `MontantPayment`,`DateLivraison`,`MoyenPayment`,`ID_Facture`) " + "VALUES('" + this->getDatePaiment() + "', '" + this->getMontantPayment() + "', '" + this->getDateLivraison() + "', '" + this->getMoyenPayment() + "','" + ID_Facture + "');SELECT @@IDENTITY;";
+	}
+
 
 	String^ Commande::UPDATE(void)
 	{
-		return "UPDATE Commande " + "SET DateCommande = '" + this->getDateCommande() + "',  DateLivraison = '" + this->getDateLivraison() + "',  DatePayement = '" + this->getDatePayement() + "',  MoyenPayement = '" + this->getMoyenPayement() + "',  DateSolde = '" + this->getDateSolde() + "', MontantHT = '" + this->getMontantHT() + "',  MontantTVA = '" + this->getMontantTVA() + "',MontantTTC = '" + this->getMontantTTC() + "'" + "WHERE(Reference = " + this->getReference() + ");";
+		return "UPDATE `region` " +
+			"INNER JOIN `Facture` ON `Date`.`ID_Facture` = `Facture`.`ID_Facture`" +
+			"INNER JOIN `Commande` ON `Facture`.`ID_Facture` = `Commande`.`ID_Facture`" +
+			"SET" +
+			"`Commande`.`QuantitéArticle` = '" + this->getQuantitéArticle() + "'," +
+			"`Commande`.`PrixUnitaire` = '" + this->getPrixUnitaire() + "'," +
+			"`Facture`.`RefCommande` = '" + this->getRefCommande() + "'," +
+			"`Facture`.`MontantTVA` = '" + this->getMontantTVA() + "'," +
+			"`Facture`.`DateLivraison` = '" + this->getDateLivraison() + "'," +
+			"`Facture`.`Remise` = '" + this->getRemise() + "'," +
+			"`Facture`.`DateSolde` = '" + this->getDateSolde() + "'," +
+			"`Date`.`DatePaiment` = '" + this->getDatePaiment() + "'," +
+			"`Date`.`MontantPayment` = '" + this->getMontantPayment() + "'," +
+			"`Date`.`MoyenPayment` = '" + this->getMoyenPayment() + "'" +
+			"WHERE" +
+			"`Commande`.`ID_Facture` = " + this->getID_Facture() + ";";
 	}
 
 	String^ Commande::DELETE(void)
 	{
-		return "DELETE FROM Commande " + "WHERE(Reference = " + this->getReference() + ");";
+		return "DELETE FROM Commande " + "WHERE(ID_Facture = " + this->getID_Facture() + ");";
 	}
 
-	void Commande::setReference(String^ Reference)
+	void Commande::setID_Facture(int Reference)
 	{
-		if (Reference != "")
+		if (ID_Facture >0)
 		{
-			this->Reference = Reference;
+			this->ID_Facture = ID_Facture;
 		}
 		
 	}
 
-	void Commande::setDateCommande(String^ DateCommande)
+	void Commande::setID_Article(int ID_Article)
 	{
-		if (DateCommande != "")
+		if (ID_Article >0)
 		{
-			this->DateCommande = DateCommande;
+			this->ID_Article = ID_Article;
 		}
 	}
 
-	void Commande::setDateLivraison(String^ DateLivraison)
+	void Commande::setQuantitéArticle(int QuantitéArticle)
 	{
-		if (DateLivraison != "")
+		if (QuantitéArticle > 0)
 		{
-			this->DateLivraison = DateLivraison;
+			this->QuantitéArticle = QuantitéArticle;
 		}
 	}
-	void Commande::setDatePayement(String^ DatePayement)
+	void Commande::setPrixUnitaire(int PrixUnitaire)
 	{
-		if (DatePayement != "")
+		if (PrixUnitaire >0)
 		{
-			this->DatePayement = DatePayement;
+			this->PrixUnitaire = PrixUnitaire;
 		}
 	}
-	void Commande::setMoyenPayement(String^ MoyenPayement)
+	void Commande::setMoyenPayment(String^ MoyenPayment)
 	{
-		if (MoyenPayement != "")
+		if (MoyenPayment != "")
 		{
-			this->MoyenPayement = MoyenPayement;
+			this-> MoyenPayment = MoyenPayment;
 		}
 	}
 	void Commande::setDateSolde(String^ DateSolde)
@@ -81,11 +101,11 @@ namespace NS_Composants
 			this->DateSolde = DateSolde;
 		}
 	}
-	void Commande::setMontantHT(int MontantHT)
+	void Commande::setMontantPayment(int MontantPaiment)
 	{
-		if (MontantHT > 0)
+		if (MontantPaiment > 0)
 		{
-			this->MontantHT = MontantHT;
+			this->MontantPayment = MontantPaiment;
 		}
 	}
 	void Commande::setMontantTVA(int MontantTVA)
@@ -95,50 +115,83 @@ namespace NS_Composants
 			this->MontantTVA = MontantTVA;
 		}
 	}
-	void Commande::setMontantTTC(int MontantTTC)
+	void Commande::setRefCommande(String^ RefCommande)
 	{
-		if (MontantTTC > 0)
+		if (RefCommande != "")
 		{
-			this->MontantTTC = MontantTTC;
+			this->RefCommande = RefCommande;
 		}
 	}
-	
-
-	String^ Commande::getReference(void)
+	void Commande::setDateLivraison(String^ DateLivraison)
 	{
-		return this->Reference;
+		if (DateLivraison != "")
+		{
+			this->DateLivraison = DateLivraison;
+		}
+	}
+	void Commande::setRemise(int Remise)
+	{
+		if (Remise > 0)
+		{
+			this-> Remise= Remise;
+		}
+	}
+	void Commande::setDatePaiment(String^ DatePaiment)
+	{
+		if (DatePaiment != "")
+		{
+			this->DatePaiment = DatePaiment;
+		}
+	}
+	int Commande::getID_Facture(void)
+	{
+		return this->ID_Facture;
 	}
 
-	String^ Commande::getDateCommande(void)
+	int Commande::getID_Article(void)
 	{
-		return this->DateCommande;
+		return this->ID_Article;
 	}
 	String^ Commande::getDateLivraison(void)
 	{
 		return this->DateLivraison;
 	}
-	String^ Commande::getDatePayement(void)
+	String^ Commande::getDatePaiment(void)
 	{
-		return this->DatePayement;
+		return this->DatePaiment;
 	}
-	String^ Commande::getMoyenPayement(void)
+	String^ Commande::getMoyenPayment(void)
 	{
-		return this->MoyenPayement;
+		return this->MoyenPayment;
 	}
 	String^ Commande::getDateSolde(void)
 	{
 		return this->DateSolde;
 	}
-	int Commande::getMontantHT(void)
+	int Commande::getMontantPayment(void)
 	{
-		return this->MontantHT;
+		return this->MontantPayment;
+	}
+	int Commande::getQuantitéArticle(void)
+	{
+		return this->QuantitéArticle;
+	}
+	int Commande::getPrixUnitaire(void)
+	{
+		return this->PrixUnitaire;
+	}
+	String^ Commande::getRefCommande(void)
+	{
+		return this->RefCommande;
+	}
+	int Commande::getRemise(void)
+	{
+		return this-> Remise;
 	}
 	int Commande::getMontantTVA(void)
 	{
 		return this->MontantTVA;
 	}
-	int Commande::getMontantTTC(void)
-	{
-		return this->MontantTTC;
-	}
 }
+
+
