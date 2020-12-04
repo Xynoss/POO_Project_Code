57930680c4181431e87db497ee5c1d1e10eed710
Bleidwyn <74651888+Bleidwyn@users.noreply.github.com>
2020-12-04 11:24:09 +0100

    Modification service gestion commande
    Adaptation de la classe service de gestion des commandes

--- Projet_POO_Application/Projet_POO_Application/CL_svc_gestionCommande.h
+++ Projet_POO_Application/Projet_POO_Application/CL_svc_gestionCommande.h
@@ -1,28 +1,26 @@
 #pragma once
 #include "CL_CAD.h"
-#include "Commande.h"
+#include "CL_map_Commande.h"
 #include "CL_map_TBCLIENT.h"
-
 using namespace System::Data;
 using namespace MySql::Data::MySqlClient;
 using namespace System::Xml;
 using namespace System;
 namespace NS_Svc {
-	ref class CL_Svc_commande
+	ref class Svc_commande
 	{
 	private:
 		NS_Composants::CL_CAD^ cad;
 		NS_Composants::CL_map_Commande^ Commande;
-		NS_Composants::CL_map_TBCLIENT^ ClientClé;
 		DataSet^ DS;
 		DataTable^ DT;
 	public:
-
-		CL_Svc_commande(void);
+		Svc_commande(void);
 		DataTable^ TableCommande();
 		DataSet^ listeCommande(String^);
-		String^ ajouter(String^, String^, String^, String^, String^, String^,int, int, int);
-		void modifier(String^, String^, String^, String^, String^, String^,int , int, int);
-		void supprimer(String^);
+		int ajouter(String^, String^, String^, String^, String^, int, int, int, int);
+		void modifier(int, String^, String^, String^, String^, String^, int, int, int);
+		void supprimer(int);
 	};
 }
+
