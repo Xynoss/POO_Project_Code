895a5f9fe203772ceb703254545f38fc59c24972
Bleidwyn <74651888+Bleidwyn@users.noreply.github.com>
2020-12-07 13:33:16 +0100

    Préparation plusieurs articles

--- Projet_POO_Application/Projet_POO_Application/MyForm.h
+++ Projet_POO_Application/Projet_POO_Application/MyForm.h
@@ -134,6 +134,17 @@ private: System::Windows::Forms::TextBox^ textBox17;
 
 private: System::Windows::Forms::Label^ label1;
 private: System::Windows::Forms::Label^ label17;
+private: System::Windows::Forms::TextBox^ textBox1;
+private: System::Windows::Forms::TextBox^ textBox2;
+private: System::Windows::Forms::TextBox^ textBox3;
+private: System::Windows::Forms::TextBox^ textBox4;
+private: System::Windows::Forms::Label^ label2;
+private: System::Windows::Forms::Label^ label3;
+private: System::Windows::Forms::Label^ label4;
+private: System::Windows::Forms::Label^ label5;
+private: System::Windows::Forms::GroupBox^ groupBox1;
+private: System::Windows::Forms::Button^ button1;
+private: System::Windows::Forms::Button^ button2;
 
 
 
@@ -212,8 +223,20 @@ private: System::Windows::Forms::Label^ label17;
             this->textBox17 = (gcnew System::Windows::Forms::TextBox());
             this->label1 = (gcnew System::Windows::Forms::Label());
             this->label17 = (gcnew System::Windows::Forms::Label());
+            this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+            this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+            this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+            this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+            this->label2 = (gcnew System::Windows::Forms::Label());
+            this->label3 = (gcnew System::Windows::Forms::Label());
+            this->label4 = (gcnew System::Windows::Forms::Label());
+            this->label5 = (gcnew System::Windows::Forms::Label());
+            this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+            this->button1 = (gcnew System::Windows::Forms::Button());
+            this->button2 = (gcnew System::Windows::Forms::Button());
             (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
             this->groupTable->SuspendLayout();
+            this->groupBox1->SuspendLayout();
             this->SuspendLayout();
             // 
             // lbl_id
@@ -701,11 +724,113 @@ private: System::Windows::Forms::Label^ label17;
             this->label17->TabIndex = 52;
             this->label17->Text = L"label17";
             // 
+            // textBox1
+            // 
+            this->textBox1->Location = System::Drawing::Point(65, 15);
+            this->textBox1->Name = L"textBox1";
+            this->textBox1->Size = System::Drawing::Size(100, 20);
+            this->textBox1->TabIndex = 53;
+            // 
+            // textBox2
+            // 
+            this->textBox2->Location = System::Drawing::Point(65, 41);
+            this->textBox2->Name = L"textBox2";
+            this->textBox2->Size = System::Drawing::Size(100, 20);
+            this->textBox2->TabIndex = 54;
+            // 
+            // textBox3
+            // 
+            this->textBox3->Location = System::Drawing::Point(65, 67);
+            this->textBox3->Name = L"textBox3";
+            this->textBox3->Size = System::Drawing::Size(100, 20);
+            this->textBox3->TabIndex = 55;
+            // 
+            // textBox4
+            // 
+            this->textBox4->Location = System::Drawing::Point(65, 94);
+            this->textBox4->Name = L"textBox4";
+            this->textBox4->Size = System::Drawing::Size(100, 20);
+            this->textBox4->TabIndex = 56;
+            // 
+            // label2
+            // 
+            this->label2->AutoSize = true;
+            this->label2->Location = System::Drawing::Point(4, 17);
+            this->label2->Name = L"label2";
+            this->label2->Size = System::Drawing::Size(35, 13);
+            this->label2->TabIndex = 57;
+            this->label2->Text = L"label2";
+            // 
+            // label3
+            // 
+            this->label3->AutoSize = true;
+            this->label3->Location = System::Drawing::Point(4, 42);
+            this->label3->Name = L"label3";
+            this->label3->Size = System::Drawing::Size(35, 13);
+            this->label3->TabIndex = 58;
+            this->label3->Text = L"label3";
+            // 
+            // label4
+            // 
+            this->label4->AutoSize = true;
+            this->label4->Location = System::Drawing::Point(4, 68);
+            this->label4->Name = L"label4";
+            this->label4->Size = System::Drawing::Size(35, 13);
+            this->label4->TabIndex = 59;
+            this->label4->Text = L"label4";
+            // 
+            // label5
+            // 
+            this->label5->AutoSize = true;
+            this->label5->Location = System::Drawing::Point(4, 94);
+            this->label5->Name = L"label5";
+            this->label5->Size = System::Drawing::Size(35, 13);
+            this->label5->TabIndex = 60;
+            this->label5->Text = L"label5";
+            // 
+            // groupBox1
+            // 
+            this->groupBox1->Controls->Add(this->button1);
+            this->groupBox1->Controls->Add(this->label5);
+            this->groupBox1->Controls->Add(this->label4);
+            this->groupBox1->Controls->Add(this->label3);
+            this->groupBox1->Controls->Add(this->label2);
+            this->groupBox1->Controls->Add(this->textBox4);
+            this->groupBox1->Controls->Add(this->textBox3);
+            this->groupBox1->Controls->Add(this->textBox2);
+            this->groupBox1->Controls->Add(this->textBox1);
+            this->groupBox1->Location = System::Drawing::Point(18, 666);
+            this->groupBox1->Name = L"groupBox1";
+            this->groupBox1->Size = System::Drawing::Size(295, 124);
+            this->groupBox1->TabIndex = 61;
+            this->groupBox1->TabStop = false;
+            this->groupBox1->Text = L"Article";
+            // 
+            // button1
+            // 
+            this->button1->Location = System::Drawing::Point(206, 88);
+            this->button1->Name = L"button1";
+            this->button1->Size = System::Drawing::Size(75, 23);
+            this->button1->TabIndex = 61;
+            this->button1->Text = L"Valider";
+            this->button1->UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this->button2->Location = System::Drawing::Point(268, 601);
+            this->button2->Name = L"button2";
+            this->button2->Size = System::Drawing::Size(84, 28);
+            this->button2->TabIndex = 62;
+            this->button2->Text = L"Ajouter article";
+            this->button2->UseVisualStyleBackColor = true;
+            // 
             // FRM_Principal
             // 
             this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
             this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
             this->ClientSize = System::Drawing::Size(1852, 896);
+            this->Controls->Add(this->button2);
+            this->Controls->Add(this->groupBox1);
             this->Controls->Add(this->label17);
             this->Controls->Add(this->label1);
             this->Controls->Add(this->textBox17);
@@ -760,6 +885,8 @@ private: System::Windows::Forms::Label^ label17;
             (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
             this->groupTable->ResumeLayout(false);
             this->groupTable->PerformLayout();
+            this->groupBox1->ResumeLayout(false);
+            this->groupBox1->PerformLayout();
             this->ResumeLayout(false);
             this->PerformLayout();
 
