ba48ad9a54d64fe9de77ccacff46a27998ca1174
Xynoss <74651875+Xynoss@users.noreply.github.com>
2020-12-08 13:44:41 +0100

    test reboot

---
+++ Projet_POO_Application/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Projet_POO_Application/CL_map_TBPERSONNEL.h"
+#include "../Projet_POO_Application/CL_svc_gestionPersonnel.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		
+		}
+	};
+}
